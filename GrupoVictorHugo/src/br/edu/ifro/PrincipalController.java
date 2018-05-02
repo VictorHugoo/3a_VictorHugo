d179d7380abea6553fa056fa1462befad2c204ff
Victor Hugo da Rocha Pires <victorpires666@gmail.com>
2018-05-02 11:13:14 -0400

    Começando a calculadora

--- GrupoVictorHugo/src/br/edu/ifro/PrincipalController.java
+++ GrupoVictorHugo/src/br/edu/ifro/PrincipalController.java
@@ -10,7 +10,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +20,14 @@ import javafx.scene.control.Label;
 public class PrincipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNumero1, txtNumero2, txtResultado;
+    
+    @FXML Button btnSoma, btnSubtracao, btnMultiplicacao, btnDivisao;
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void Soma(ActionEvent event) {
+       
     }
     
     @Override
