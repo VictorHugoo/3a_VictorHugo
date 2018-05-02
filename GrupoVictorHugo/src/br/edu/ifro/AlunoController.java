08f8e78f2aed6fcfeccf5d1b269d144df4c2c9ee
Victor Hugo da Rocha Pires <victorpires666@gmail.com>
2018-05-02 14:47:10 -0400

    Criando um MenuBar com o cadastro de ALunos

---
+++ GrupoVictorHugo/src/br/edu/ifro/AlunoController.java
@@ -0,0 +1,16 @@
+
+package br.edu.ifro;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class AlunoController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- GrupoVictorHugo/src/br/edu/ifro/PrincipalController.java
+++ GrupoVictorHugo/src/br/edu/ifro/PrincipalController.java
@@ -5,24 +5,34 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author 03194090232
  */
 public class PrincipalController implements Initializable {
+
+    private TextField txtNumero1;
     
     @FXML
-    private TextField txtNumero1, txtNumero2, txtResultado;
+    private TextField txtNumero2, txtResultado;
+    Button btnSoma;
     
-    @FXML Button btnSoma, btnSubtracao, btnMultiplicacao, btnDivisao;
+    @FXML Button btnSubtracao, btnMultiplicacao, btnDivisao;
+    @FXML
+    private Label label;
     
     
     @FXML
@@ -34,7 +44,6 @@ public class PrincipalController implements Initializable {
        txtResultado.setText(resultado.toString());
     }
     
-    @FXML
     private void Subtracao(ActionEvent event) {
        Double  num1 = Double.parseDouble(txtNumero1.getText());
        Double  num2 = Double.parseDouble(txtNumero2.getText());
@@ -42,7 +51,6 @@ public class PrincipalController implements Initializable {
        
        txtResultado.setText(resultado.toString());
     }
-    @FXML
     private void Multiplicacao(ActionEvent event) {
        Double  num1 = Double.parseDouble(txtNumero1.getText());
        Double  num2 = Double.parseDouble(txtNumero2.getText());
@@ -50,7 +58,6 @@ public class PrincipalController implements Initializable {
        
        txtResultado.setText(resultado.toString());
     }
-    @FXML
     private void Divisao(ActionEvent event) {
        Double  num1 = Double.parseDouble(txtNumero1.getText());
        Double  num2 = Double.parseDouble(txtNumero2.getText());
@@ -63,5 +70,24 @@ public class PrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void AbrirCadastroAluno(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 900, 682);
+            Stage stage = new Stage();
+            stage.setTitle("Cadastrar Alunos");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+
+}
+    }
+
+    @FXML
+    private void FecharProjeto(ActionEvent event) {
+    }
     
 }
